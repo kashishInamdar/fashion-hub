711ce63cfaae2811ad45abba2ddf7c311d756e20
kashishInamdar <kashishinamdar2004@gmail.com>
2023-11-05 13:51:18 +0530

    SignUp Integrate

--- server/index.js
+++ server/index.js
@@ -211,7 +211,7 @@ app.get("/products/search" , async (req , res)=>{
     })
 });
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 8080; 
 
 app.listen(PORT , ()=>{
     console.log(`Server is Running on Port : ${PORT}`)
